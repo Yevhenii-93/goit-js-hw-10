26ead4e11e7b200944493e3f50cf7599c4d55cf9
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-03-13 05:00:13 +0200

    1

---
+++ src/js/2-snakebar.js
@@ -0,0 +1,61 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+// Напиши скрипт, який після сабміту форми створює проміс. В середині колбека цього промісу через вказану користувачем кількість мілісекунд проміс має виконуватися (при fulfilled) або відхилятися (при rejected), залежно від обраної опції в радіокнопках. Значенням промісу, яке передається як аргумент у методи resolve/reject, має бути значення затримки в мілісекундах.
+
+// Створений проміс треба опрацювати у відповідних для вдалого/невдалого виконання методах.
+
+// Якщо проміс виконується вдало, виводь у консоль наступний рядок, де delay — це значення затримки виклику промісу в мілісекундах.
+
+// `✅ Fulfilled promise in ${delay}ms`;
+
+// Якщо проміс буде відхилено, то виводь у консоль наступний рядок, де delay — це значення затримки промісу в мілісекундах.
+
+// `❌ Rejected promise in ${delay}ms`
+
+const inputDelay = document.querySelector('input[name="delay"]');
+const inputFulfilled = document.querySelector('input[value="fulfilled"]');
+const inputRejected = document.querySelector('input[value="rejected"]');
+const btnSubmit = document.querySelector('btn[type="submit"]');
+
+btnSubmit.addEventListener('submit', sendDelay);
+function sendDelay(event) {
+  event.preventDefault();
+
+  const delayValue = Number(inputDelay.value);
+  const isInputFulfilled = inputFulfilled.checked;
+  const isInputRejected = inputRejected.checked;
+
+  return userDelayValue(delayValue, isInputFulfilled, isInputRejected);
+}
+
+function userDelayValue(delayValue, isInputFulfilled, isInputRejected) {
+  let promise = new Promise((resolve, reject) => {
+    if (isInputFulfilled) {
+      setTimeout(() => {
+        resolve();
+      }, delayValue);
+    }
+    if (isInputRejected) {
+      setTimeout(() => {
+        reject();
+      }, delayValue);
+    }
+  });
+}
+
+promise
+  .then(resolve => {
+    iziToast.success({
+      color: 'green',
+      position: 'topRight',
+      message: `✅ Fulfilled promise in ${timer}ms`,
+    });
+  })
+  .catch(reject => {
+    iziToast.error({
+      color: 'red',
+      position: 'topRight',
+      message: `❌ Rejected promise in ${timer}ms`,
+    });
+  });
